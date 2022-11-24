1fd47f4ba34f6d5a09404185f5b520ca6634d140
Kee Jefferys <kee@loki.network>
2022-11-25 02:15:43 +1100

    move try catch
    move to prevent issue where entire org would be skipped

--- VueRefactor/serverSide/scripts/commitAggregator.js
+++ VueRefactor/serverSide/scripts/commitAggregator.js
@@ -8,19 +8,21 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 var githubToken = "";
-var commitRepoArray=[]
+var commitRepoArray = [];
 var requestCounter = 0;
 
 var repoList = JSON.parse(readFileFromDisk());
 
 for (let index = 0; index < repoList.length; index++) {
-    const element = repoList[index];
-    console.log("fetching data for coin Name: " + element.title);
-    var coinData = await fetchList(element);
-    commitRepoArray.push(coinData);
+  const element = repoList[index];
+  console.log("fetching data for coin Name: " + element.title);
+  var coinData = await fetchList(element);
+  commitRepoArray.push(coinData);
 }
-writeToFile(path.resolve(__dirname, "../assets/weeklyCoinCommitNumber.txt"),JSON.stringify(commitRepoArray));
-
+writeToFile(
+  path.resolve(__dirname, "../assets/weeklyCoinCommitNumber.txt"),
+  JSON.stringify(commitRepoArray)
+);
 
 function readFileFromDisk() {
   try {
@@ -35,21 +37,22 @@ function readFileFromDisk() {
 }
 
 async function fetchList(repoList) {
-    var coinObject = {}
-    var weeklyCommitSummation = Array(52).fill(0);
-  try {
-    for (let index = 0; index < repoList.repositories.length; index++) {
+  var coinObject = {};
+  var weeklyCommitSummation = Array(52).fill(0);
+
+  for (let index = 0; index < repoList.repositories.length; index++) {
+    try {
       const element = repoList.repositories[index];
       var splitURL = element.split("/");
-      console.log(repoList.title +": "+ splitURL[4] + " " + requestCounter);
+      console.log(repoList.title + ": " + splitURL[4] + " " + requestCounter);
 
-      requestCounter++
+      requestCounter++;
       if (requestCounter == 4500) {
-        //sleep for 60 mins 
-        console.log("sleeping for 60 mins")
-        await new Promise(r => setTimeout(r, 3600000));
+        //sleep for 60 mins
+        console.log("sleeping for 60 mins");
+        await new Promise((r) => setTimeout(r, 3600000));
         requestCounter = 0;
-      }  
+      }
       //lets check to see if we are getting a 200 or 202
       var response = await axios.get(
         `https://api.github.com/repos/${splitURL[3]}/${splitURL[4]}/stats/participation`,
@@ -59,28 +62,27 @@ async function fetchList(repoList) {
           },
         }
       );
-      
+
       for (let index = 0; index < response.data.all.length; index++) {
         const element = response.data.all[index];
         weeklyCommitSummation[index] += element;
       }
+
+    } catch (error) {
+      console.log(error);
+      console.log("error fetching data for coin Name: " + repoList.title);
+
+      if (error.response.data.message.includes("API rate limit exceeded")) {
+        console.log("sleeping for 60 mins");
+        await new Promise((r) => setTimeout(r, 3600000));
+      }
     }
 
-    coinObject.title = repoList.title
-    coinObject.weeklyCommitSummation = weeklyCommitSummation
-    return coinObject
-
-  } catch (error) {
-    console.log(error);
-    console.log("error fetching data for coin Name: " + repoList.title);
-    
-    if (error.response.data.message.includes("API rate limit exceeded")) {
-      console.log("sleeping for 60 mins")
-      await new Promise(r => setTimeout(r, 3600000));
-    }
-      
-    }
   }
+  coinObject.title = repoList.title;
+  coinObject.weeklyCommitSummation = weeklyCommitSummation;
+  return coinObject;
+}
 
 function writeToFile(path, data) {
   try {
